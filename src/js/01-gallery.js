50d01caf5b6b6584ddb1bd79d29477595a5a5915
Sergey Shishkin <Shishkin_SU@ukr.net>
2022-07-23 17:35:47 +0300

    task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,43 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+// galleryContainer.addEventListener('click', onGalleryItemsClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${original}"
+          
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+}
+
+function onTextareaInput(evt) {
+  const message = evt.currentTarget.value;
+
+  localStorage.setItem('fbk-message', message);
+  console.log(message);
+}
