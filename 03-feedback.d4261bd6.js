a1db9018d4580eaebd889d4d8f8513e17c15820d
Stavr101 <95885361+Stavr101@users.noreply.github.com>
2022-07-25 10:20:28 +0000

    Deploying to gh-pages from @ Stavr101/goit-js-hw-08@e99cc0d8464a32893282ebdf37ad40a3d8415a3a 🚀

--- 03-feedback.d4261bd6.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset()})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("fbk-message",t),console.log(t)}));
-//# sourceMappingURL=03-feedback.d4261bd6.js.map
